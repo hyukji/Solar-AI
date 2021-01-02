8c4368413d109c0126fa293d2c3ad483a1a34a4d
DH CHOI <vectorch99@gmail.com>
2021-01-02 16:10:54 +0900

    add comment

--- analysis_data.py
+++ analysis_data.py
@@ -6,9 +6,15 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
 trainCsv = pd.read_csv("./data/train/train.csv")
-list_zero = trainCsv[trainCsv["DNI"] == 0].index
+# DNI = 0 제거
+list_zero = trainCsv[trainCsv["DHI"] == 0].index
 removeZero=  trainCsv.drop(list_zero)
-shuffled=removeZero.iloc[np.random.permutation(range(500))].reset_index(drop=True)
-sns.regplot(x="WS",y="TARGET", data=shuffled[:500])
+shuffled=removeZero.iloc[np.random.permutation(range(500))]
+
+sns.regplot(x="DHI",y="TARGET", data=shuffled[:500])
 # %%
-sns.distplot(shuffled["DNI"])
+# 0 제거 전
+sns.distplot(trainCsv["DHI"])
+# %%
+# 0 제거 후
+sns.distplot(shuffled["DHI"])
